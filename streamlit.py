0fdd09de6561247c757e8017d4d846d239b245ce
kuofunshing <163220723+kuofunshing@users.noreply.github.com>
2024-06-25 17:08:32 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -12,11 +12,12 @@ api_key = os.getenv("OPENAI_API_KEY")
 if not api_key:
     raise ValueError("Please set the OPENAI_API_KEY environment variable.")
 
-# 建立 YouTube API 客户端
+# 建立 YouTube API 客戶端
 def get_youtube_client():
     api_key = st.secrets["YOUTUBE_API_KEY"]
     youtube = build('youtube', 'v3', developerKey=api_key)
     return youtube
+
 # 搜索 YouTube 影片
 def youtube_search(query, max_results=5):
     youtube = get_youtube_client()
@@ -34,7 +35,6 @@ def youtube_search(query, max_results=5):
         video_url = f"https://www.youtube.com/watch?v={video_id}"
         results.append((video_title, video_url))
     return results
-
 
 # 初始化 OpenAI 客戶端
 openai.api_key = api_key
@@ -150,27 +150,27 @@ def create_user(username, password):
     conn.close()
 
 def image_processing():
-    st.header("图片")
-    st.write("这是图片页面。")
+    st.header("圖片")
+    st.write("這是圖片頁面。")
     options = ["Bus", "Car", "Cheetah", "Penguins", "Pig", "Scooter", "cat", "rabbit", "zebra"]
-    animal = st.selectbox("选择一个项目", options)
+    animal = st.selectbox("選擇一個項目", options)
 
-    # 初始化 session state，用于跟踪用户的选择和操作状态
+    # 初始化 session state，用於跟踪用戶的選擇和操作狀態
     if 'last_viewed_animal' not in st.session_state:
         st.session_state['last_viewed_animal'] = None
 
-    confirm_button = st.button("确认")
+    confirm_button = st.button("確認")
 
-    # 当用户点击确认按钮并且所选项目改变时，显示图片和文本
+    # 當用戶點擊確認按鈕並且所選項目改變時，顯示圖片和文本
     if confirm_button:
         if animal != st.session_state['last_viewed_animal']:
             st.session_state['last_viewed_animal'] = animal
-            # 扣除次数
+            # 扣除次數
             st.session_state['remaining_uses'] -= 1
-            st.success("次数已扣除，您可以查看图片。")
+            st.success("次數已扣除，您可以查看圖片。")
             display_image_and_text(animal)
         else:
-            st.success("您已查看此图片，次数不再扣除。")
+            st.success("您已查看此圖片，次數不再扣除。")
             display_image_and_text(animal)
 
 def display_image_and_text(animal):
@@ -179,41 +179,42 @@ def display_image_and_text(animal):
 
     if os.path.exists(image_path) and os.path.exists(text_path):
         image = Image.open(image_path)
-        st.image(image, caption=f'显示的是: {animal}', use_column_width=True)
+        st.image(image, caption=f'顯示的是: {animal}', use_column_width=True)
 
         with open(text_path, 'r') as file:
             text_content = file.read()
         st.write(text_content)
     else:
-        st.error("文件不存在，请确保路径和文件名正确。")
+        st.error("文件不存在，請確保路徑和文件名正確。")
 
-    uploaded_file = st.file_uploader("选择一个图片文件", type=["jpg", "jpeg", "png"])
+    uploaded_file = st.file_uploader("選擇一個圖片文件", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption='上传的图片', use_column_width=True)
+        st.image(image, caption='上傳的圖片', use_column_width=True)
     else:
-        st.write("请上传一个图片文件。")
+        st.write("請上傳一個圖片文件。")
+
 # 充值頁面
 def recharge_page():
-    st.header("充值页面")
-    st.write("这是充值页面。")
+    st.header("充值頁面")
+    st.write("這是充值頁面。")
     
-    card_number = st.text_input("卡号", type="password", max_chars=16)
+    card_number = st.text_input("卡號", type="password", max_chars=16)
     
     months = [f"{i:02d}" for i in range(1, 13)]
     years = [f"{i:02d}" for i in range(0, 25)]
     
-    selected_month = st.selectbox("选择月份", months)
-    selected_year = st.selectbox("选择年份", years)
+    selected_month = st.selectbox("選擇月份", months)
+    selected_year = st.selectbox("選擇年份", years)
     
     month_year = f"{selected_month}/{selected_year}"
     
     cvv = st.text_input("CVV", max_chars=3)
-    amount_option = st.selectbox("选择充值金额", ["10次,100元", "100次,9990元", "1000次,99900元"])
+    amount_option = st.selectbox("選擇充值金額", ["10次,100元", "100次,9990元", "1000次,99900元"])
     
     if st.button("充值"):
-        # 检查卡号长度是否为16位
+        # 檢查卡號長度是否為16位
         if len(card_number) == 16 and card_number.isdigit() and cvv.isdigit() and len(cvv) == 3 and amount_option:
             amount_map = {
                 "10次,100元": 10,
@@ -221,20 +222,20 @@ def recharge_page():
                 "1000次,99900元": 1000
             }
             st.session_state['remaining_uses'] += amount_map[amount_option]
-            st.success("充值成功！剩余服务次数已增加。")
+            st.success("充值成功！剩餘服務次數已增加。")
         else:
             if len(card_number) != 16:
-                st.error("卡号必须是16位数字。")
+                st.error("卡號必須是16位數字。")
             else:
-                st.error("请填写所有必填字段，并确保CVV为3位数字。")
+                st.error("請填寫所有必填字段，並確保CVV為3位數字。")
 
 def yt_page():
     st.header("YT頁面")
     st.write("這是YT頁面。")
-    st.title("搜索和显示 YouTube 影片")
+    st.title("搜索和顯示 YouTube 影片")
 
     # 搜索 YouTube 影片
-    query = st.text_input("输入关键词搜索 YouTube 影片:")
+    query = st.text_input("輸入關鍵字搜索 YouTube 影片:")
     if st.button("搜索"):
         if query:
             search_results = youtube_search(query)
@@ -242,12 +243,12 @@ def yt_page():
                 for title, url in search_results:
                     st.markdown(f"[{title}]({url})")
             else:
-                st.write("没有找到相关视频。")
+                st.write("沒有找到相關視頻。")
         else:
-            st.warning("请输入搜索关键词。")
+            st.warning("請輸入搜索關鍵字。")
 
-    # 显示预设的视频选项
-    st.write("或选择以下预设视频观看：")
+    # 顯示預設的視頻選項
+    st.write("或選擇以下預設視頻觀看：")
     video_options = {
         "影片 1": "https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=74s&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D",
         "影片 2": "https://www.youtube.com/watch?v=tJuJ0Dls1hI&ab_channel=%E9%88%BE%E9%88%A6%E4%BA%BA%E6%AF%92%E6%B0%A3%E9%81%8E%E5%BA%A6%E9%9C%80%E8%A6%81",
@@ -260,35 +261,35 @@ def yt_page():
         "影片 9": "https://www.youtube.com/watch?v=mdSXKdnLX9I&pp=ygUP57SF6JOu44Gu5byT55-i",
     }
 
-
-    selected_video = st.selectbox("选择一个影片", list(video_options.keys()))
-    if st.button("播放选定影片"):
+    selected_video = st.selectbox("選擇一個影片", list(video_options.keys()))
+    if st.button("播放選定影片"):
         if not st.session_state['has_played'] or st.session_state['last_played_video'] != selected_video:
             st.session_state['remaining_uses'] -= 1
             st.session_state['has_played'] = True
             st.session_state['last_played_video'] = selected_video
-            st.success("播放次数扣除")
+            st.success("播放次數扣除")
         else:
-            st.success("已播放过此影片，不再扣次")
+            st.success("已播放過此影片，不再扣次")
         
         st.video(video_options[selected_video])
+
 # GPT頁面
 def gpt_page():
     st.title("GPT Chatbot-yt推薦")
-    st.write("与 ChatGPT 进行对话，获取基于标签的YouTube關鍵字推荐。")
+    st.write("與 ChatGPT 進行對話，獲取基於標籤的YouTube關鍵字推薦。")
 
-    # 初始化聊天历史记录
+    # 初始化聊天歷史記錄
     if 'chat_history' not in st.session_state:
         st.session_state['chat_history'] = []
 
-    # 获取用户输入
+    # 獲取用戶輸入
     user_input = st.text_input("你：", key="input")
 
-    # 当用户输入新消息时，将其添加到聊天历史记录中并获取模型的响应
+    # 當用戶輸入新消息時，將其添加到聊天歷史記錄中並獲取模型的響應
     if user_input:
         st.session_state['chat_history'].append({"role": "user", "content": user_input})
     
-        # 添加系统信息指导模型行为
+        # 添加系統信息指導模型行為
         system_message = "你是影片搜尋助手,以繁體中文回答,請根據提供的標籤提供四組用於youtube搜尋的關鍵字"
         st.session_state['chat_history'].append({"role": "system", "content": system_message})
 
@@ -296,16 +297,14 @@ def gpt_page():
             chat_completion = client.chat.completions.create(
                 messages=st.session_state['chat_history'],
                 model="gpt-4o",
-                max_tokens=200  # 设置最大token数为200
+                max_tokens=200  # 設置最大token數為200
             )
             assistant_message = chat_completion.choices[0].message.content
             st.session_state['chat_history'].append({"role": "assistant", "content": assistant_message})
         except Exception as e:
             st.error(f"An error occurred: {str(e)}")
 
-
-
-    # 显示聊天历史记录
+    # 顯示聊天歷史記錄
     for message in st.session_state['chat_history']:
         role = "你" if message["role"] == "user" else "ChatGPT"
         st.write(f"{role}: {message['content']}")
